a7d5c5ffc1186eb41c5740d3ef55ee23af27b73a
yxqsnz <yxqsnz@gmail.com>
2022-12-18 12:33:02 -0300

    Cors:: allow everyone

--- src/app.rs
+++ src/app.rs
@@ -4,7 +4,7 @@ use axum::{
     Router,
 };
 use sqlx::{Pool, Sqlite};
-use tower_http::cors::CorsLayer;
+use tower_http::cors::{AllowHeaders, CorsLayer};
 
 use crate::endpoints::{asset, by, meta, random};
 pub fn endpoints(pool: Pool<Sqlite>) -> Router {
@@ -14,15 +14,9 @@ pub fn endpoints(pool: Pool<Sqlite>) -> Router {
         .route("/api/v1/by/:type/:category", get(by::endpoint))
         .route("/api/v1/asset/:name", get(asset::endpoint))
         .layer(
-            // see https://docs.rs/tower-http/latest/tower_http/cors/index.html
-            // for more details
-            //
-            // pay attention that for some request types like posting content-type: application/json
-            // it is required to add ".allow_headers([http::header::CONTENT_TYPE])"
-            // or see this issue https://github.com/tokio-rs/axum/issues/849
-            // python3 -m http.server 8000 in `static`
             CorsLayer::new()
-                .allow_origin("http://localhost:8000".parse::<HeaderValue>().unwrap())
+                .allow_origin("*".parse::<HeaderValue>().unwrap())
+                .allow_headers(AllowHeaders::any())
                 .allow_methods([Method::GET]),
         )
         .with_state(pool)
