feb387ae43b3de0731a4ec2f682ed0eb2da73e52
yxqsnz <yxqsnz@gmail.com>
2022-12-21 17:19:06 -0300

    Fix /api/v2/:category behavior

--- src/endpoints/v2/img.rs
+++ src/endpoints/v2/img.rs
@@ -21,17 +21,26 @@ pub struct Params {
 
 pub async fn get(
     Connection(mut conn): Connection,
-    Path((category,)): Path<(String,)>,
+    Path(category): Path<String>,
     Query(params): Query<Params>,
 ) -> RESTResult<Json<Response>> {
-    let (name, provider, nsfw): (String, String, bool) = sqlx::query_as(
-        "select name, provider, nsfw from asset where nsfw=? and category=? order by random() limit 1",
-    )
-    .bind(params.nsfw.unwrap_or(false))
-    .bind(category)
-    .fetch_one(&mut conn)
-    .await
-    .map_err(not_found_or_500)?;
+    let query = match params.nsfw {
+        Some(nsfw) => sqlx::query_as(
+            "select name, provider, nsfw from asset \
+                               where nsfw=? and category=? order by random() limit 1",
+        )
+        .bind(nsfw),
+        None => sqlx::query_as(
+            "select name, provider, nsfw from asset \
+            where category=? order by random() limit 1",
+        ),
+    };
+
+    let (name, provider, nsfw): (String, String, bool) = query
+        .bind(category)
+        .fetch_one(&mut conn)
+        .await
+        .map_err(not_found_or_500)?;
 
     Ok(Json(Response {
         url: format!("{}/api/v1/asset/{name}", var("Api.Url").unwrap_or_default()),
